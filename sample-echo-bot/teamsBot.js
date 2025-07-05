e86bffafd1676a979014a4c9376e41476000d500
iamjoker021 <iamjoker021@gmail.com>
2025-07-05 18:07:38 +0530

    Add sample-echo-bot to Learn repo

---
+++ sample-echo-bot/teamsBot.js
@@ -0,0 +1,74 @@
+const { ActivityTypes } = require("@microsoft/agents-activity");
+const {
+  AgentApplication,
+  AttachmentDownloader,
+  MemoryStorage,
+} = require("@microsoft/agents-hosting");
+const { version } = require("@microsoft/agents-hosting/package.json");
+
+const downloader = new AttachmentDownloader();
+
+// Define storage and application
+const storage = new MemoryStorage();
+const teamsBot = new AgentApplication({
+  storage,
+  fileDownloaders: [downloader],
+});
+
+// Listen for user to say '/reset' and then delete conversation state
+teamsBot.message("/reset", async (context, state) => {
+  state.deleteConversationState();
+  await context.sendActivity("Ok I've deleted the current conversation state.");
+});
+
+teamsBot.message("/count", async (context, state) => {
+  const count = state.conversation.count ?? 0;
+  await context.sendActivity(`The count is ${count}`);
+});
+
+teamsBot.message("/diag", async (context, state) => {
+  await state.load(context, storage);
+  await context.sendActivity(JSON.stringify(context.activity));
+});
+
+teamsBot.message("/state", async (context, state) => {
+  await state.load(context, storage);
+  await context.sendActivity(JSON.stringify(state));
+});
+
+teamsBot.message("/runtime", async (context, state) => {
+  const runtime = {
+    nodeversion: process.version,
+    sdkversion: version,
+  };
+  await context.sendActivity(JSON.stringify(runtime));
+});
+
+teamsBot.conversationUpdate("membersAdded", async (context, state) => {
+  await context.sendActivity(
+    `Hi there! I'm an echo bot running on Agents SDK version ${version} that will echo what you said to me.`
+  );
+});
+
+// Listen for ANY message to be received. MUST BE AFTER ANY OTHER MESSAGE HANDLERS
+teamsBot.activity(ActivityTypes.Message, async (context, state) => {
+  // Increment count state
+  let count = state.conversation.count ?? 0;
+  state.conversation.count = ++count;
+
+  // Echo back users request
+  await context.sendActivity(`[${count}] you said: ${context.activity.text}`);
+});
+
+teamsBot.activity(/^message/, async (context, state) => {
+  await context.sendActivity(`Matched with regex: ${context.activity.type}`);
+});
+
+teamsBot.activity(
+  async (context) => Promise.resolve(context.activity.type === "message"),
+  async (context, state) => {
+    await context.sendActivity(`Matched function: ${context.activity.type}`);
+  }
+);
+
+module.exports.teamsBot = teamsBot;
